cab4b4e62530ae669ef70b4c9ed36bf60cc473dd
Yilin Ma <yilinma3@illinois.edu>
2020-10-05 22:08:25 -0500

    uncommented View all comments button

--- src/components/PostPotraitCommentContainter.jsx
+++ src/components/PostPotraitCommentContainter.jsx
@@ -4,18 +4,41 @@ import '../styles/PostPotraitCommentContainter.css';
 import PostViewText from './PostViewText';
 import {connect} from 'react-redux';
 import {toggleLikeComment} from '../actions/commentActions';
-
+import {Link} from 'react-router-dom';
 class PostPotraitCommentContainter extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            commentDisplayOffset: 0,
+        }
+    }
+
+    componentDidMount() {
+        // determine where in comment object to start render
+        const initalCommentCount = Object.keys(this.props.comments).length;
+        const commentDisplayOffset = Math.max(0,initalCommentCount - 2);
+        this.setState({commentDisplayOffset: commentDisplayOffset});
+    }
+
     render() {
+        const commentCount = Object.keys(this.props.comments).length;
         return (
             <section className="PostPotraitCommentContainter">
-                {/* <div className="ViewAllCommentsButton Button">
-                        View all 133 comments
-                </div> */}
+                { commentCount > 2 ? 
+                    <Link to="/landscape" style={{ textDecoration: 'none' }}>
+                        <div className="ViewAllCommentsButton Button">
+                        View all {commentCount} comments
+                        </div> 
+                    </Link> : null
+                }
                 <div className="PostPotraitCommentList">
                     {
                         Object.entries(this.props.comments)
                             .map(([key, val], idx) => {
+                                if (idx < this.state.commentDisplayOffset) {
+                                    return null;
+                                }
                                 return PostViewText(true, this.props.toggleLikeComment, key, val, idx);
                             })
                     }
